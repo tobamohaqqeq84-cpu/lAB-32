e9a77ec44e462fe60d089e30925713fd7703d0b6
tobam <tobamohaqqeq84@gmail.com>
2025-11-15 01:57:53 +0000

    2nd

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,3 +1,19 @@
 //Toba Mohaqqeq
 //COMSC-210
 //lab-32
+
+#include <iostream>
+#include <deque>
+#include <cstdlib>
+#include <ctime>
+#include "Car.h"
+using namespace std;
+
+int main(){
+    const int INITAL_CARS = 2;
+    const int PAY_PROBABILITY = 55;  //55% PAY
+
+    srand(time(0));
+    
+}
+
